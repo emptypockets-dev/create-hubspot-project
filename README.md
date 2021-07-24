1efe051ee4f88eee5584c4617071e4087738ec25
Andrey Kondratyuk <andrey.vw@gmail.com>
2021-07-24 09:45:55 -0400

    more setup

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # create-hubspot-project
+
+## Not ready yet. Do not use or share.
+
+🏗 Personal CLI to bootstrap new Hubspot projects
+
+## Installation & Usage
+
+```bash
+npm init create-hubspot-project
+# or
+npx create-hubspot-project
+# or
+npm install -g create-hubspot-project
+create-hubspot-project
+```
+
+## License
+
+MIT
+
+## Collaborators
+
+- Andrey Kondratyuk <andrey@fasterbetter.dev>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,44 @@
 import chalk from 'chalk';
+import execa from 'execa';
 import fs from 'fs';
+import gitignore from 'gitignore';
+import Listr from 'listr';
 import ncp from 'ncp';
 import path from 'path';
-import { promisify } from 'util';
-import execa from 'execa';
-import Listr from 'listr';
 import { projectInstall } from 'pkg-install';
+import license from 'spdx-license-list/licenses/MIT';
+import { promisify } from 'util';
 
 const access = promisify(fs.access);
+const writeFile = promisify(fs.writeFile);
+const copy = promisify(ncp);
+const writeGitignore = promisify(gitignore.writeFile);
+
 const copy = promisify(ncp);
 
 async function copyTemplateFiles(options) {
   return copy(options.templateDirectory, options.targetDirectory, {
     clobber: false,
   });
+}
+
+async function createGitignore(options) {
+  const file = fs.createWriteStream(
+    path.join(options.targetDirectory, '.gitignore'),
+    { flags: 'a' }
+  );
+  return writeGitignore({
+    type: 'Node',
+    file: file,
+  });
+}
+
+async function createLicense(options) {
+  const targetPath = path.join(options.targetDirectory, 'LICENSE');
+  const licenseContent = license.licenseText
+    .replace('<year>', new Date().getFullYear())
+    .replace('<copyright holders>', `${options.name} (${options.email})`);
+  return writeFile(targetPath, licenseContent, 'utf8');
 }
 
 async function initGit(options) {
@@ -30,11 +55,13 @@ export async function createProject(options) {
   options = {
     ...options,
     targetDirectory: options.targetDirectory || process.cwd(),
+    email: 'andrey@fasterbetter.dev',
+    name: 'Andrey Kondratyuk',
   };
 
-  const currentFileUrl = import.meta.url;
+  const fullPathName = new URL(import.meta.url).pathname;
   const templateDir = path.resolve(
-    new URL(currentFileUrl).pathname,
+    fullPathName.substr(fullPathName.indexOf('/')),
     '../../templates',
     options.template.toLowerCase()
   );
@@ -47,28 +74,41 @@ export async function createProject(options) {
     process.exit(1);
   }
 
-  const tasks = new Listr([
+  const tasks = new Listr(
+    [
+      {
+        title: 'Copy project files',
+        task: () => copyTemplateFiles(options),
+      },
+      {
+        title: 'Create gitignore',
+        task: () => createGitignore(options),
+      },
+      {
+        title: 'Create License',
+        task: () => createLicense(options),
+      },
+      {
+        title: 'Initialize git',
+        task: () => initGit(options),
+        enabled: () => options.git,
+      },
+      {
+        title: 'Install dependencies',
+        task: () =>
+          projectInstall({
+            cwd: options.targetDirectory,
+          }),
+        skip: () =>
+          !options.runInstall
+            ? 'Pass --install to automatically install dependencies'
+            : undefined,
+      },
+    ],
     {
-      title: 'Copy project files',
-      task: () => copyTemplateFiles(options),
-    },
-    {
-      title: 'Initialize git',
-      task: () => initGit(options),
-      enabled: () => options.git,
-    },
-    {
-      title: 'Install dependencies',
-      task: () =>
-        projectInstall({
-          cwd: options.targetDirectory,
-        }),
-      skip: () =>
-        !options.runInstall
-          ? 'Pass --install to automatically install dependencies'
-          : undefined,
-    },
-  ]);
+      exitOnError: false,
+    }
+  );
 
   await tasks.run();
   console.log('%s Project ready', chalk.green.bold('DONE'));
